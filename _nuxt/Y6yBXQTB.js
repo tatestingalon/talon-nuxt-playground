f6264615919317efce733391c9a66b845c1f1c99
tatestingalon <talon05.work@gmail.com>
2025-07-31 00:17:27 +0000

    Deploying to gh-pages from @ tatestingalon/talon-nuxt-playground@110cec1d241bb67b08948a9e9d140166714904ad 🚀

---
+++ _nuxt/Y6yBXQTB.js
@@ -0,0 +1,1 @@
+import{_ as s,d as a,i as r,O as t}from"./C9GCVUbQ.js";const n={};function o(e,c){return r(),a("table",null,[t(e.$slots,"default")])}const _=Object.assign(s(n,[["render",o]]),{__name:"ProseTable"});export{_ as default};
